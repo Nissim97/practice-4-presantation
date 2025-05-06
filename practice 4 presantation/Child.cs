c2d3c028fc1389fbb791539f1b2ea4f97fdf1247
nissim mekonen <nissmekonen@gmail.com>
2025-05-06 12:14:33 +0300

    Add project files.

---
+++ practice 4 presantation/Child.cs
@@ -0,0 +1,29 @@
+﻿namespace practice_4_presantation
+{
+    public class Child
+    {
+        private string name;
+        private int age;
+        private bool isMale;
+
+        public Child(string name, bool isMale)
+        {
+            this.name = name;
+            this.isMale = isMale;
+        }
+
+        private int PalculateAge(int year)
+        {
+            int currentYear = 2025;
+
+            this.age = 2025 - year;
+
+            return this.age;
+        }
+
+        protected void PrintInfo()
+        {
+            Console.WriteLine($"this child name is {name} he is {age} years old and the fact that its a male is {isMale}");
+        }
+    }
+}
---
+++ practice 4 presantation/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace practice_4_presantation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Child child1 = new Child("nissim", true);
+
+        }
+    }
+
+
+}
